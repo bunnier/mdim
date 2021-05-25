279e50dca57f6fe00b24dd5817ca8fc831ec6a20
bunnier <zrui@outlook.com>
2021-05-25 22:43:32 +0800

    Refine the goroutine sync logic in deleting images.

--- core/image_cleanup.go
+++ core/image_cleanup.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"sync"
 
 	"mdim/core/types"
 )
@@ -12,7 +11,6 @@ import (
 // DeleteNoRefImgs Iterate imageFolder to find & delete no reference images.
 func DeleteNoRefImgs(absImgFolder string, allRefImgsAbsPathSet types.Set, doImgDel bool) []types.ImageHandleResult {
 	handleResultCh := make(chan types.ImageHandleResult)
-	wg := sync.WaitGroup{}
 	count := 0 // The count of handling files.
 	filepath.WalkDir(absImgFolder, func(imgPath string, d os.DirEntry, err error) error {
 		if d.IsDir() {
@@ -23,11 +21,13 @@ func DeleteNoRefImgs(absImgFolder string, allRefImgsAbsPathSet types.Set, doImgD
 			return nil
 		}
 
-		wg.Add(1)
 		count++
 		go func() {
-			defer wg.Done()
 			handleResult := types.ImageHandleResult{ImagePath: imgPath}
+			defer func() {
+				// Ensure to pass handling result to main goroutine, otherwise can cause deadlock.
+				handleResultCh <- handleResult
+			}()
 			if doImgDel {
 				if err := os.Remove(imgPath); err != nil {
 					handleResult.Err = fmt.Errorf("delete no referemce image failed\n%w", err)
@@ -36,28 +36,19 @@ func DeleteNoRefImgs(absImgFolder string, allRefImgsAbsPathSet types.Set, doImgD
 					handleResult.Deleted = true
 				}
 			}
-			// Pass handling result to main goroutine.
-			handleResultCh <- handleResult
 		}()
 
 		return nil
 	})
 
-	// Waiting for all goroutine done to close channel.
-	go func() {
-		wg.Wait()
-		close(handleResultCh)
-	}()
-
 	// handling result receiver
 	handleResultSlice := make([]types.ImageHandleResult, 0, count)
-	for {
-		handleResult, chOpen := <-handleResultCh
-		if !chOpen {
-			break
-		}
+	for count > 0 {
+		handleResult := <-handleResultCh
 		handleResultSlice = append(handleResultSlice, handleResult)
+		count--
 	}
+	close(handleResultCh)
 
 	return handleResultSlice
 }
