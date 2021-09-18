7d15d8099b138a6d2546d43df9b8d13a0b08291c
bunnier <zrui@outlook.com>
2021-09-18 23:15:33 +0800

    improve qiniu interact codes

--- internal/qiniu_upload.go
+++ internal/qiniu_upload.go
@@ -2,6 +2,7 @@ package internal
 
 import (
 	"context"
+	"errors"
 	"time"
 
 	"github.com/qiniu/go-sdk/v7/auth/qbox"
@@ -9,66 +10,100 @@ import (
 )
 
 // Qiniu SDK doc: https://developer.qiniu.com/kodo/1238/go
+
+// QiniuUploadApi is a wrapper upload api for Qiniu SDK
 type QiniuUploadApi struct {
 	ak              string
 	sk              string
 	bucket          string
-	domains         []string
+	domain          string
 	token           string
 	tokenExpireTime time.Time
+
+	mac           *qbox.Mac
+	bucketManager *storage.BucketManager
 }
 
-func NewQuniuUploadApi(ak string, sk string, bucket string) *QiniuUploadApi {
+type QiniuUploadApiOption func(api *QiniuUploadApi)
+
+func QiniuUploadApiDomainOption(domain string) QiniuUploadApiOption {
+	return func(api *QiniuUploadApi) {
+		api.domain = domain
+	}
+}
+
+func NewQuniuUploadApi(ak, sk, bucket string, options ...QiniuUploadApiOption) *QiniuUploadApi {
 	api := &QiniuUploadApi{
 		ak:     ak,
 		sk:     sk,
 		bucket: bucket,
+		mac:    qbox.NewMac(ak, sk),
 	}
+
+	cfg := storage.Config{
+		UseHTTPS: true,
+	}
+	api.bucketManager = storage.NewBucketManager(api.mac, &cfg)
+
+	for _, option := range options {
+		option(api)
+	}
+
+	if api.domain == "" {
+		if err := api.fetchDomain(); err != nil {
+			panic(err)
+		}
+	}
+
 	api.refreshToken()
+
 	return api
 }
 
+// refreshToken
 func (api *QiniuUploadApi) refreshToken() {
-	if api.tokenExpireTime.Add(-120 * time.Second).After(time.Now()) {
+	const timeoutOffset time.Duration = 120 * time.Second
+	if api.tokenExpireTime.Add(-timeoutOffset).After(time.Now()) {
 		return
 	}
+
 	// Get token by sdk.
 	putPolicy := storage.PutPolicy{
 		Scope: api.bucket,
 	}
-	mac := qbox.NewMac(api.ak, api.sk)
-	api.token = putPolicy.UploadToken(mac)
+	api.token = putPolicy.UploadToken(api.mac)
 }
 
-// Upload file
-func (api *QiniuUploadApi) Upload(remoteFilepath, localFilepath string) error {
+// fetchDomains of the specific bucket.
+func (api *QiniuUploadApi) fetchDomain() error {
+	var domainInfos []storage.DomainInfo
+	var err error
+	if domainInfos, err = api.bucketManager.ListBucketDomains(api.bucket); err != nil {
+		return err
+	}
+	for _, di := range domainInfos {
+		if di.Domain == "" {
+			continue
+		}
+		api.domain = di.Domain
+		return nil
+	}
+	return errors.New("no available domain")
+}
+
+// Upload file to Quniu cloud, and then return the public url.
+func (api *QiniuUploadApi) Upload(remoteFilepath, localFilepath string) (string, error) {
+	return api.UploadContext(context.Background(), remoteFilepath, localFilepath)
+}
+
+// Upload file to Quniu cloud, and then return the public url.
+func (api *QiniuUploadApi) UploadContext(ctx context.Context, remoteFilepath, localFilepath string) (string, error) {
 	api.refreshToken()
 	cfg := storage.Config{}
 	formUploader := storage.NewFormUploader(&cfg)
 	ret := storage.PutRet{}
-	return formUploader.PutFile(context.Background(), &ret, api.token, remoteFilepath, localFilepath, nil)
-}
-
-func (api *QiniuUploadApi) GetDomains() ([]string, error) {
-	mac := qbox.NewMac(api.ak, api.sk)
-	cfg := storage.Config{
-		UseHTTPS: true,
+	if err := formUploader.PutFile(ctx, &ret, api.token, remoteFilepath, localFilepath, nil); err != nil {
+		return "", err
 	}
-
-	bucketManager := storage.NewBucketManager(mac, &cfg)
-	var domainInfos []storage.DomainInfo
-	var err error
-	if domainInfos, err = bucketManager.ListBucketDomains(api.bucket); err != nil {
-		return nil, err
-	}
-
-	domains := make([]string, len(domainInfos))
-	for _, di := range domainInfos {
-		if di.Domain != "" {
-			continue
-		}
-		domains = append(domains, di.Domain)
-	}
-	api.domains = domains
-	return api.domains, nil
+	return storage.MakePublicURL(api.domain, remoteFilepath), nil
 }
--- internal/qiniu_upload_test.go
+++ internal/qiniu_upload_test.go
@@ -1,15 +1,18 @@
 package internal
 
 import (
+	"net/url"
 	"path/filepath"
+	"strings"
 	"testing"
 )
 
 const (
-	ak          string = ""
-	sk          string = ""
-	bucket      string = ""
-	testImgPath string = ""
+	ak          string = "RHAGGu151y6ZaE_D_Ds3eMG_ozLk9hok1nui2f9X"
+	sk          string = "hR4Ni1V85EPXSH5PWkueiHyM14JEjtX_HmHl7pDE"
+	bucket      string = "1024baby"
+	testImgPath string = "/Users/zhongrui/Desktop/儿童语言发展.jpg"
+	testDomain  string = "http://img.1024baby.com"
 )
 
 func TestQiniuUploadApi_Upload(t *testing.T) {
@@ -25,36 +28,22 @@ func TestQiniuUploadApi_Upload(t *testing.T) {
 	}{
 		{
 			"test1",
-			NewQuniuUploadApi(ak, sk, bucket),
+			NewQuniuUploadApi(ak, sk, bucket, QiniuUploadApiDomainOption(testDomain)),
 			args{remoteFilepath: filepath.Base(testImgPath), localFilepath: testImgPath},
 			false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if err := tt.api.Upload(tt.args.remoteFilepath, tt.args.localFilepath); (err != nil) != tt.wantErr {
+			wantUrl := strings.Join([]string{tt.api.domain, url.PathEscape(tt.args.remoteFilepath)}, "/")
+			var url string
+			var err error
+			if url, err = tt.api.Upload(tt.args.remoteFilepath, tt.args.localFilepath); (err != nil) != tt.wantErr {
 				t.Errorf("QiniuUploadApi.Upload() error = %v, wantErr %v", err, tt.wantErr)
 			}
-		})
-	}
-}
 
-func TestQiniuUploadApi_GetDomains(t *testing.T) {
-	tests := []struct {
-		name    string
-		api     *QiniuUploadApi
-		wantErr bool
-	}{
-		{
-			"test1",
-			NewQuniuUploadApi(ak, sk, bucket),
-			false,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if _, err := tt.api.GetDomains(); (err != nil) != tt.wantErr {
-				t.Errorf("QiniuUploadApi.GetDomains() error = %v, wantErr %v", err, tt.wantErr)
+			if wantUrl != url {
+				t.Errorf("QiniuUploadApi.Upload() url = %v, wantUrl %v", url, wantUrl)
 			}
 		})
 	}
